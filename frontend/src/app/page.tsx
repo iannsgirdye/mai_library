ff46ea4b553521e80188c2ba98885cde7e32c725
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-05-11 03:10:10 +0300

    Fix and update a bit in general page
    - Hero: link to MAI site.
    - Pages: blue line under title, animation fixed.
    - Other:
      - ../frontend/**/HomeHero.tsx -> ../frontend/**/Hero.tsx;
      - ../frontend/**/HomePages.tsx -> ../frontend/**/Pages.tsx.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,9 @@
 import { Metadata } from "next";
-import HomeHero from "@/components/HomeHero";
-import HomePages from "@/components/HomePages"
+import HomeHero from "@/components/Hero";
+import HomePages from "@/components/Pages"
 
 export const metadata: Metadata = {
-  title: "mai_library — библиотека маёвца",
+  title: "mai_library — cборник материалов для маёвца",
   description: "Сборник файлов и ресурсов, которые обязательно пригодятся студенту Московского авиационного института во время учебной и внеучебной деятельностей.",
 };
 
---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+export default function HomeHero() {
+  return (
+    <section className="text-center py-16 fade-in bg-blue-50">
+      <div className="container mx-auto px-4">
+        <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-4">mai_library</h1>
+        <p className="text-xl text-gray-600 max-w-4xl mx-auto">
+          Сборник файлов и ресурсов, которые обязательно пригодятся студенту <a href="https://mai.ru">Московского авиационного института</a> во время учебной и внеучебной деятельностей.
+        </p>
+      </div>
+    </section>
+  );
+}
